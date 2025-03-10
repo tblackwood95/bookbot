e6236758569b1c0ab296cee8bcfa774bfbc3527f
tblackwood95 <tblackwood@live.com>
2025-03-10 01:31:19 -0230

    added functions character_count and sorted_dict and formatted the look of the bookbot

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
 from stats import num_words
+from stats import character_count
+from stats import sorted_dict
 
 def main():
     text = get_book_text("books/frankenstein.txt")
     word_count = num_words(text)
-    print(f"{word_count} words found in the document")
+    characters = character_count(text)
+    print(f"============ BOOKBOT ============")
+    print(f"Analyzing book found at books/frankenstein.txt...")
+    print(f"----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print(f"--------- Character Count -------")
+    for char_dict in sorted_dict(characters):
+        if char_dict["char"].isalpha():
+            print(f"{char_dict["char"]}: {char_dict["count"]}")
 
+#def main():
+    #text = get_book_text("books/frankenstein.txt")
+    #word_count = num_words(text)
+    #char_counts = character_count(text)
+    #print(f"{word_count} words found in the document")
+    #for char, count in char_counts.items():
+        #print(f"'{char}': {count}")
 
 def get_book_text(filepath):
     with open(filepath) as f:
--- stats.py
+++ stats.py
@@ -1,3 +1,20 @@
 def num_words(text):
     words = text.split()
     return len(words)
+
+def character_count(text):
+    characters = {}
+    lower_text = text.lower()
+    for letter in lower_text:
+        if letter in characters:
+            characters[letter] += 1
+        else:
+            characters[letter] = 1
+    return characters
+
+def sorted_dict(characters):
+    sorted_list = []
+    for letter, count in characters.items():
+        sorted_list.append({"char": letter, "count": count})
+    sorted_list.sort(key=lambda x: (-x["count"], x["char"]))
+    return sorted_list
