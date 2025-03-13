def5bf4e615a0330748b0a6b6ac79b3151eba2f6
tblackwood95 <tblackwood@live.com>
2025-03-12 22:16:17 -0230

    Update main.py

--- main.py
+++ main.py
@@ -19,13 +19,6 @@ def main():
             print(f"{char_dict["char"]}: {char_dict["count"]}")
     print(f"============= END ===============")
 
-#def main():
-    #text = get_book_text("books/frankenstein.txt")
-    #word_count = num_words(text)
-    #char_counts = character_count(text)
-    #print(f"{word_count} words found in the document")
-    #for char, count in char_counts.items():
-        #print(f"'{char}': {count}")
 
 def get_book_text(filepath):
     with open(filepath) as f:
